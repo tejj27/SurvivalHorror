5b467e4d8e71fed495f33f4415c2edf3269d2854
Tejal <tjsathe2002@gmail.com>
2022-03-10 19:06:31 +0530

    Rifles and Revolver done firing animations

--- Survival Horror/Assets/player/PlayerScripts/PlayerAttack.cs
+++ Survival Horror/Assets/player/PlayerScripts/PlayerAttack.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+
+    public WeaponManager weapon_manager;
+
+    public float firerate = 15f;
+    private float nextTimeToFire;
+
+    private float damage = 20f;
+
+
     // Start is called before the first frame update
+
+    private void Awake() {
+        weapon_manager = GetComponent<WeaponManager>();
+    }
     void Start()
     {
         
@@ -13,6 +26,52 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        WeaponShoot();
+    }
+
+    void WeaponShoot()
+    {
+        //if we have a assualt rifle
+        if(weapon_manager.GetCurrentSelectedWeapon().FireType == WeaponFireType.MULTIPLE)
+        {
+            //if we pressed and hold the left mouse button down 0
+            if(Input.GetMouseButton(0) && Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f /firerate;
+                weapon_manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                //bulletFired
+            }
+        }
+        else
+        {
+            // if we have regualr weapon
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                //Handle Axe
+                if(weapon_manager.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG)
+                {
+                    weapon_manager.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+
+                   //weapon Manager
+            if(weapon_manager.GetCurrentSelectedWeapon().Bullettype == WeaponBullettype.BULLET)
+            {
+                weapon_manager.GetCurrentSelectedWeapon().ShootAnimation();
+                //BulletFired()
+
+            }
+            else
+            {
+                // if we have an error
+            }
+
+            
+            }
+         
+
+        }
+
     }
 }

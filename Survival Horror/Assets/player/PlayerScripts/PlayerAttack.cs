04b72461c0a52d494c05ee5f21df170a44c48a30
Tejal <tjsathe2002@gmail.com>
2022-03-12 13:37:41 +0530

    ZOmbie Animations prob

--- Survival Horror/Assets/player/PlayerScripts/PlayerAttack.cs
+++ Survival Horror/Assets/player/PlayerScripts/PlayerAttack.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
 
-    public WeaponManager weapon_manager;
+    private WeaponManager weapon_manager;
 
     public float firerate = 15f;
     private float nextTimeToFire;
@@ -13,10 +13,31 @@ public class PlayerAttack : MonoBehaviour
     private float damage = 20f;
 
 
+    private Animator ZoomCameraAnim;
+
+    private bool Zoomed;
+
+    private Camera mainCam;
+
+    private GameObject crossHair;
+
+    private bool is_Aiming;
+
+
+
+
+
+
     // Start is called before the first frame update
 
     private void Awake() {
         weapon_manager = GetComponent<WeaponManager>();
+        //finding child  transform in player
+        ZoomCameraAnim = transform.Find(Tags.LOOK_ROOT).transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
+
+        crossHair = GameObject.FindWithTag(Tags.CROSS_HAIR); // references to the object
+
+        mainCam = Camera.main;
     }
     void Start()
     {
@@ -27,6 +48,7 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         WeaponShoot();
+        ZoomInAndOut();
     }
 
     void WeaponShoot()
@@ -40,7 +62,7 @@ public class PlayerAttack : MonoBehaviour
                 nextTimeToFire = Time.time + 1f /firerate;
                 weapon_manager.GetCurrentSelectedWeapon().ShootAnimation();
 
-                //bulletFired
+                BulletFired();
             }
         }
         else
@@ -56,22 +78,74 @@ public class PlayerAttack : MonoBehaviour
                 }
 
                    //weapon Manager
-            if(weapon_manager.GetCurrentSelectedWeapon().Bullettype == WeaponBullettype.BULLET)
-            {
+                if(weapon_manager.GetCurrentSelectedWeapon().Bullettype == WeaponBullettype.BULLET)
+                {
                 weapon_manager.GetCurrentSelectedWeapon().ShootAnimation();
-                //BulletFired()
+                BulletFired();
 
-            }
-            else
-            {
-                // if we have an error
-            }
+                }
+                else
+                {
+                 // if we have an arrow aor sphere
+                 if(is_Aiming)
+                 {
+                     //arrow and sphere
+                 }
+                }
 
-            
             }
          
 
         }
 
     }
+
+
+    void ZoomInAndOut()
+    {
+        if(weapon_manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.AIM)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                ZoomCameraAnim.Play(AnimationTags.ZOOM_IN_ANIM);
+
+                crossHair.SetActive(false);
+            }
+            if(Input.GetMouseButtonUp(1))
+            {
+                ZoomCameraAnim.Play(AnimationTags.ZOOM_OUT_ANIM);
+
+                crossHair.SetActive(true);
+            }
+        }
+
+        if(weapon_manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.SELF_AIM)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                weapon_manager.GetCurrentSelectedWeapon().AIM(true);
+                is_Aiming = true;
+            }
+            if(Input.GetMouseButtonUp(1))
+            {
+                weapon_manager.GetCurrentSelectedWeapon().AIM(false);
+                is_Aiming = false;
+            }
+        }
+
+    }// zoom in and out 
+
+
+    void BulletFired()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit))
+        {
+            Debug.DrawRay(mainCam.transform.position, mainCam.transform.forward, Color.red, 2f);
+            print("We HIT :" + hit.transform.gameObject.name);
+            
+        }
+
+    }// bullet fired
 }

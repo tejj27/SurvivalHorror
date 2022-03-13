d1381cbfe15fd5a08747cb0a484e95ac6c004f7a
Tejal <tjsathe2002@gmail.com>
2022-03-13 13:32:32 +0530

    Animations of zombie

---
+++ Survival Horror/Assets/player/EnmeyScripts/EnemyAnimator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator anim;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+
+
+
+
+    }
+
+    private void Start() {
+        Dead();
+    }
+
+    private void Walk(bool Walk)
+    {
+        anim.SetBool(AnimationTags.WALK_PARAMETER, Walk);
+    }
+
+    public void Run(bool run)
+    {
+        anim.SetBool(AnimationTags.RUN_PARAMETER, run);
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger(AnimationTags.ATTACK_TRIGGER);
+    }
+      public void Dead()
+    {
+        anim.SetTrigger(AnimationTags.DEAD_TRIGGER);
+    }
+}
